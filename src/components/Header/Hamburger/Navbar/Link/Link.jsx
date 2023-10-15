4331f0244aa03e238d58f3f0da4df076bf9b2314
anton <antonkurilik@gmail.com>
2023-10-15 19:40:05 +0300

    f

--- src/components/Header/Hamburger/Navbar/Link/Link.jsx
+++ src/components/Header/Hamburger/Navbar/Link/Link.jsx
@@ -5,19 +5,14 @@ import styles from './Link.module.scss'
 export const Link = (props) => {
   const { value, state, setState, linkTo } = props
   const { width } = useWindowSize()
- 
-
-  
 
   const handleClickLink = (e) => {
     width <= 1024 && setState(!state)
     const element = document.getElementById(linkTo)
-    console.log(element);
+ 
     element.scrollIntoView({ behavior: 'smooth' })
-      
   }
 
- 
   return (
     <div
       onClick={handleClickLink}
